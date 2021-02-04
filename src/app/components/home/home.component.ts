0397b1068dea3e2391001bd38a9d5767999cc920
Elliot Thomas <elliot.thomas@gmail.com>
2021-02-03 19:29:31 -0600

    Pulling Data from Firebase

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+// Firebase imports
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  users: Observable<any[]>;
+
+  constructor(firestore: AngularFirestore) {
+    this.users = firestore.collection('/users').valueChanges();
+  }
 
   ngOnInit(): void {
   }
