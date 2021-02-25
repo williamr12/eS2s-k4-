420a088a7a69659031f2db894726c287b45a1049
Elliot Thomas <elliot.thomas@gmail.com>
2021-02-25 09:03:00 -0600

    added production config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig : {
+    apiKey: 'AIzaSyCm2qffKvd71DVOdZsbo6yBxyXrmrrF6YY',
+    authDomain: 'providers-fyi-42d78.firebaseapp.com',
+    databaseURL: 'https://providers-fyi-42d78-default-rtdb.firebaseio.com',
+    projectId: 'providers-fyi-42d78',
+    storageBucket: 'providers-fyi-42d78.appspot.com',
+    messagingSenderId: '194735303082',
+    appId: '1:194735303082:web:658b1edf3133109ca1e3d3',
+    measurementId: 'G-5VF94TEFL0'
+  }
+
 };
